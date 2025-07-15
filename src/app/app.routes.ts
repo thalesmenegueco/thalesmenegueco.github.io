7a687593c08c104b513a46f67ba066a0188e02d2
thales <thales.menegueco@gmail.com>
2025-07-15 10:41:22 -0300

    applying best practices

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
+/*
 import { Routes } from '@angular/router';
-
 export const routes: Routes = [];
+*/
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Importar o componente
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },              // Rota /home
+  { path: '', redirectTo: '/home', pathMatch: 'full' },   // Redirecionar raiz para /home
+  { path: '**', redirectTo: '/home' }                     // Redirecionar rotas inválidas
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
