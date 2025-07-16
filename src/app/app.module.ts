57e4324fa78f9492c75ab1a154d37a72c055233c
thales <thales.menegueco@gmail.com>
2025-07-16 15:24:47 -0300

    deleting the import of Browser module in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
-    BrowserModule,
     AppRoutingModule,
     AppComponent,
     HomeComponent
