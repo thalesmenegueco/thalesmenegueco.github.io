59ab88d7557f341b143b801787f00029e3925e93
thales <thales.menegueco@gmail.com>
2025-07-21 12:20:51 -0300

    removing Test component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Importar o componente
 import { LearningGalleryComponent } from './learning-gallery/learning-gallery.component'; // Importar o componente LearningGallery
-import { Test } from './test/test'; // Importar o componente Test
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },              // Rota /home
-  { path: 'learning-gallery', component: LearningGalleryComponent }, // Rota /learning-gallery
-  { path: 'test', component: Test }, // componente para testar as rotas 
+  { path: 'learning-gallery', component: LearningGalleryComponent }, // Rota /learning-gallery 
   { path: '', redirectTo: '/home', pathMatch: 'full' },   // Redirecionar raiz para /home
   { path: '**', redirectTo: '/home' }                     // Redirecionar rotas inválidas - PROBLEMA???
 ];
