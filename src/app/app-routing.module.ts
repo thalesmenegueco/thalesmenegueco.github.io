9d8b212a3a6bc20c2a93229060bf4991af465a1a
thales <thales.menegueco@gmail.com>
2025-07-21 10:12:04 -0300

    testing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Importar o componente
 import { LearningGalleryComponent } from './learning-gallery/learning-gallery.component'; // Importar o componente LearningGallery
+import { Test } from './test/test'; // Importar o componente Test
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },              // Rota /home
   { path: 'learning-gallery', component: LearningGalleryComponent }, // Rota /learning-gallery
+  { path: 'test', component: Test }, // test component 
   { path: '', redirectTo: '/home', pathMatch: 'full' },   // Redirecionar raiz para /home
   { path: '**', redirectTo: '/home' }                     // Redirecionar rotas inválidas - PROBLEMA???
 ];
